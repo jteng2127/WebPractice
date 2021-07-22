b11277b99383afcb1cad8fbeab35ab260aa34b6d
jteng2127 <jteng2127@gmail.com>
2021-07-22 18:30:55 +0800

    init

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+
+var app = new Vue({
+	el: '#app',
+	data: {
+		test: 'adfsf',
+		seen: true
+	}
+})
+
+var app2 = new Vue({
+	el: "#app2",
+	data: {
+		todos: [
+			"124",
+			"126",
+			"129",
+		]
+	}
+})
